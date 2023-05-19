a830d649e94984e49271fe27293bccdb4f6bd483
Shubham Bisht <shubham99bisht@gmail.com>
2023-05-19 20:07:55 +0530

    Notes HTML page completed

--- userNotes/views.py
+++ userNotes/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     user = request.user
+
+    if request.method == 'POST':
+        title = request.POST['title']
+        note = request.POST['note']
+        color = request.POST['color']
+        pass
+
     if user.is_authenticated:
         return render(request, 'userNotes/index.html')
     else:
