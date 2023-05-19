3226bc9b38c81252e907a9c871afea8c17fc62d9
Shubham Bisht <shubham99bisht@gmail.com>
2023-05-19 21:09:09 +0530

    Display notes to Frontend

--- userNotes/models.py
+++ userNotes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Notes(models.Model):
+    # Fields
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=30, help_text='Note title')
+    note = models.TextField(max_length=20, help_text='Note text')
+    color = models.CharField(max_length=10, help_text='Color Hex Code')
+
+    # Metadata
+    class Meta:
+        ordering = ['timestamp']
+
+    # Methods
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.timestamp = timezone.now()
+        return super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('home', args=[str(self.id)])
+
+    def __str__(self):
+        return self.note
+    
--- userNotes/views.py
+++ userNotes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Notes
 
 # Create your views here.
 def index(request):
@@ -8,9 +9,12 @@ def index(request):
         title = request.POST['title']
         note = request.POST['note']
         color = request.POST['color']
-        pass
+        
+        newnote = Notes(user=request.user, title=title, note=note, color=color)
+        newnote.save()
 
     if user.is_authenticated:
-        return render(request, 'userNotes/index.html')
+        all_notes = Notes.objects.filter(user=request.user)
+        return render(request, 'userNotes/index.html', {'all_notes': all_notes})
     else:
         return redirect('login')
